b7760aa11f983a9c8ad0214e2c3499cbcdbaf76b
jerry <me@programmerjerry.com>
2017-08-22 20:16:18 +0800

    修改bug：problem 3 忘记计算是必输是整除的质数了；优化算法，增加注释说明

--- Project_Euler/Problem3/Problem3.lisp
+++ Project_Euler/Problem3/Problem3.lisp
@@ -3,45 +3,52 @@
 
 ;;; 判断是否是质数
 (defun is-primer (n)
+  ;; 如果是一些已知的质数直接返回t
+  (if (or
+       (= n 2)
+       (= n 3)
+       (= n 5)
+       (= n 7)
+       (= n 11)
+       (= n 13)
+       (= n 17)
+       (= n 19)
+       (= n 23)
+       (= n 29)
+       (= n 31)
+       (= n 37))
+      (return-from is-primer t))
+
   ;; 1到n之间随机取一个数字，如果能被这个数整除，那么这个数一定不是质数，
   ;; 这个是使用的概率算法，如果运气好，能非常快判断出该数字是非质数；
   ;; 运气不好时，这里取该数字的一半作为最大的测试次数，如果还是没法判断不是
   ;; 质数，就根据质数的定义一个数字挨一个数字的判断是否能整除
   (dotimes (j (round (/ n 2.0)))
     (if (> j 2)
-        (let ((r (random j)))
+        (let ((r (random n)))
           (if (> r 1)
               (if (zerop (mod n r))
                   (return-from is-primer nil))))))
+
   ;; 根据质数定义判断是否是质数
-  (let ((i 31)) ; 由于进入本函数前已经判断了该数字不能被2到29这些质数的质数整除了
+  (let ((i 3)) ; 除了2外的质数都不是偶数，这里直接从3开始
     (loop while (<= i n)
           do (if (zerop (mod n i))
-                 (if (= i n)
-                     (return t)
-                   (return nil)))
+                  (if (= i n)
+                      (return t)
+                    (return nil)))
           (setf i (1+ i)))))
 
 (defun largest-prime-factor (n)
-  (let ((i (round (sqrt n)))) 
-    (loop while (> i 1)
-          do (;; 为了加快速度，先根据已知的质数做下整除判断
-              if (and
-                  (not (zerop (mod i 2))) 
-                  (not (zerop (mod i 3)))
-                  (not (zerop (mod i 5)))
-                  (not (zerop (mod i 7)))
-                  (not (zerop (mod i 11)))
-                  (not (zerop (mod i 13)))
-                  (not (zerop (mod i 17)))
-                  (not (zerop (mod i 23)))
-                  (not (zerop (mod i 27)))
-                  (not (zerop (mod i 29)))
-                  (not (zerop (mod n i)))
-                  (is-primer1 i))
+  (let ((i (round (sqrt n)))) ; 质数因子的平方小于等于n，所以这里从平方根后的数字开始计算，按指数减少运算量
+    (loop while (> i 2)
+          do (if (and
+                  (not (zerop (mod i 2))) ; 除了2以外的偶数都是非质数，能减少一半的计算量
+                  (zerop (mod n i)) ; 必须是整除才行
+                  (is-primer i))
                  (return i))
           (setf i (- i 1)))))
 
 
-;;; CL-USER 269 > (largest-prime-factor 600851475143)
-;;; 775121
+;;; CL-USER 49 > (largest-prime-factor 600851475143)
+;;; 6857
