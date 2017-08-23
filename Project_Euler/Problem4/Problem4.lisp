bb801242c0fa5188be2363172240504b823b8258
jerry <me@programmerjerry.com>
2017-08-23 21:30:16 +0800

    Fixbug Problem 4 理解有误，题目是要求的最大回文，错误理解成了最长的回文了。

--- Project_Euler/Problem4/Problem4.lisp
+++ Project_Euler/Problem4/Problem4.lisp
@@ -11,20 +11,16 @@
 
 (defun largest-palindrome-product (n)
   (do ((n1 n (- n1 1))
-       (max-len 0) ; 记录最大的回文的长度
-       (result nil)) ; 记录回文
+       (result 0)) ; 记录回文
       ((= n1 1) result)
     (do ((n2 n (- n2 1)))
         ((= n2 1))     
-      (let* ((prod (* n1 n2))
-             (len (length (write-to-string prod))))
+      (let* ((prod (* n1 n2)))
         (if (is-palindrome prod)
-            ; 是回文的话，长度比已经记录的长，则这个是新的最长的回文
-            (if (> len max-len)
-                (progn
-                  (setf result prod)
-                  (setf max-len len))))))))
+            ; 如果是回文并且新的值比记录的最大值大，，则这个是新的最大的回文
+            (if (> prod result)
+                  (setf result prod)))))))
 
 
-;;; CL-USER 53 > (largest-palindrome-product 999)
-;;; 580085
+;;; CL-USER 82 > (largest-palindrome-product 999)
+;;; 906609
